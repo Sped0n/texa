6878885a92ea156d5a471112e6d95360f572a1f4
Spedon Wen <realsped0n@outlook.com>
2024-10-26 01:11:31 +0800

    feat: enhance image handling with drag-and-drop functionality
    - Add drag-and-drop functionality for image handling
    - Refactor paste handler to handle individual files
    - Introduce new SolidJS components and signals for drag-and-drop state management
    - Enhance UI with a drag-and-drop overlay
    
    Signed-off-by: Spedon Wen <realsped0n@outlook.com>

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,11 @@
 import { useStore } from "@nanostores/solid";
-import { createEffect, onMount } from "solid-js";
+import {
+	Show,
+	createEffect,
+	createMemo,
+	createSignal,
+	onMount,
+} from "solid-js";
 
 import Editor from "./components/editor";
 import Image from "./components/image";
@@ -9,6 +15,7 @@ import TopBar from "./components/topbar/layout";
 
 import { setImageDataUrl } from "./stores/image";
 import { setErrMsg } from "./stores/msg";
+import { $isSettingsOpen } from "./stores/settings";
 import {
 	$isModelExist,
 	$pywebviewReady,
@@ -24,19 +31,60 @@ const App = () => {
 	const pywebviewReady = useStore($pywebviewReady);
 	const runnable = useStore($runnable);
 	const isModelExist = useStore($isModelExist);
+	const isSettingsOpen = useStore($isSettingsOpen);
+	const [isDragging, setIsDragging] = createSignal(false);
 
-	const pasteHandler = async (event: ClipboardEvent) => {
-		if (event.clipboardData === null) return;
+	const dragEnabled = createMemo(() => {
+		return !isSettingsOpen() && isDragging();
+	});
+
+	const handleFile = async (file: File) => {
 		if (!runnable()) return;
+		if (!file.type.startsWith("image/")) return;
+
 		const reader = new FileReader();
 		reader.onloadend = async () => {
 			setImageDataUrl(reader.result as string);
 			await infer(reader.result as string);
 		};
-		for (const file of event.clipboardData.files) {
+		reader.readAsDataURL(file);
+	};
+
+	const pasteHandler = async (e: ClipboardEvent) => {
+		e.preventDefault();
+		if (e.clipboardData === null) return;
+		if (!runnable()) return;
+
+		for (const file of e.clipboardData.files) {
 			if (!file.type.startsWith("image/")) continue;
-			reader.readAsDataURL(file);
+			await handleFile(file);
 			break;
+		}
+	};
+
+	const dragEnterHandler = (e: DragEvent) => {
+		e.preventDefault();
+		setIsDragging(true);
+	};
+
+	const dragLeaveHandler = (e: DragEvent) => {
+		e.preventDefault();
+		setIsDragging(false);
+	};
+
+	const dragOverHandler = (e: DragEvent) => {
+		e.preventDefault();
+		setIsDragging(true);
+	};
+
+	const dropHandler = async (e: DragEvent) => {
+		e.preventDefault();
+		setIsDragging(false);
+
+		if (!e.dataTransfer) return;
+		const file = e.dataTransfer.files[0];
+		if (file) {
+			await handleFile(file);
 		}
 	};
 
@@ -48,14 +96,14 @@ const App = () => {
 		if (window.pywebview !== undefined) {
 			setTimeout(() => {
 				setPywebviewReady(true);
-			}, 100); // delay to prevent start up lag
+			}, 100);
 		} else {
 			window.addEventListener(
 				"pywebviewready",
 				() => {
 					setTimeout(() => {
 						setPywebviewReady(true);
-					}, 100); // delay to prevent start up lag
+					}, 100);
 				},
 				{ once: true },
 			);
@@ -83,7 +131,18 @@ const App = () => {
 	});
 
 	return (
-		<div class="h=fit min-h-screen bg-gray-100 font-sans font-medium flex flex-col">
+		<div
+			class="h-fit min-h-screen bg-gray-100 font-sans font-medium flex flex-col relative"
+			onDragEnter={dragEnterHandler}
+			onDragLeave={dragLeaveHandler}
+			onDragOver={dragOverHandler}
+			onDrop={dropHandler}
+		>
+			<Show when={dragEnabled()}>
+				<div class="absolute inset-0 bg-blue-900 bg-opacity-75 flex items-center border-dashed border-white border-4 rounded-lg justify-center z-10 pointer-events-none">
+					<p class="text-white font-bold text-3xl">Drop image here</p>
+				</div>
+			</Show>
 			<TopBar />
 			<div class="flex-grow flex flex-col overflow-hidden">
 				<Image />
